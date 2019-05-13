2d88d5b11bbd86822a159b6a1729b1c4ebe9c9e1
Lindson França <l1nds0n@hotmail.com>
2019-05-13 11:53:24 -0300

    Adicionando botão FAB

--- SMTSIS/src/app/core/navbar-principal/navbar-principal.component.ts
+++ SMTSIS/src/app/core/navbar-principal/navbar-principal.component.ts
@@ -7,16 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarPrincipalComponent implements OnInit {
 
-options = {};
+  options = {};
 
   constructor() { }
 
   ngOnInit() {
     this.navbars();
+    this.fab();
   }
   navbars(){
 
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, this.options);
+  }
+
+  fab(){
+    let elems = document.querySelectorAll('.fixed-action-btn');
+    let instances = M.FloatingActionButton.init(elems, {
+      toolbarEnabled: true
+    });
   }
 }
