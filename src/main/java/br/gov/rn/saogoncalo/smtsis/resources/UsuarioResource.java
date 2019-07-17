678c2ffc7309f8b01c4cfbbe5cad57d84a161d9b
Lindson França <l1nds0n@hotmail.com>
2019-07-17 12:00:51 -0300

    Somente corrigi as importações

--- src/main/java/br/gov/rn/saogoncalo/smtsis/resources/UsuarioResource.java
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/resources/UsuarioResource.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("")
