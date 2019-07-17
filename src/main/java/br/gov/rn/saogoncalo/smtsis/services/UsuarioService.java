41d9f7fe1841f2467a779acae4eceaa7309ccffa
Lindson França <l1nds0n@hotmail.com>
2019-07-17 11:59:55 -0300

    Mudei a função para existsById

--- src/main/java/br/gov/rn/saogoncalo/smtsis/services/UsuarioService.java
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/services/UsuarioService.java
@@ -5,17 +5,19 @@ import br.gov.rn.saogoncalo.smtsis.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 
 @Service
+@Transactional
 public class UsuarioService{
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
     public Usuario buscarPeloId(Long id){
-        if (ContemUsuario(id)){
+
+        if (usuarioRepository.existsById(id)){
           Usuario usuarioEncontrado = usuarioRepository.findById(id).get();
           return usuarioEncontrado;
         }
