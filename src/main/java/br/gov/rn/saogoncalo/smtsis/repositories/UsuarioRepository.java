0e056ba228b94cd079183daed42757f5e01029fd
Lindson França <l1nds0n@hotmail.com>
2019-07-18 14:53:53 -0300

    Tentando fazer a Query para buscar usuários ativos

--- src/main/java/br/gov/rn/saogoncalo/smtsis/repositories/UsuarioRepository.java
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/repositories/UsuarioRepository.java
@@ -2,11 +2,15 @@ package br.gov.rn.saogoncalo.smtsis.repositories;
 
 import br.gov.rn.saogoncalo.smtsis.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @Query("SELECT * FROM usuarios WHERE ativo = 1")
+    List<Usuario> findByAtivo();
 
 }
