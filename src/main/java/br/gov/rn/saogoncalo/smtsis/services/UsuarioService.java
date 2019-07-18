d7e4d1742ddef3e30dd80d0270cc7c30188bf25a
Lindson França <l1nds0n@hotmail.com>
2019-07-18 10:30:10 -0300

    Serviços implementados

--- src/main/java/br/gov/rn/saogoncalo/smtsis/services/UsuarioService.java
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/services/UsuarioService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -14,6 +15,33 @@ public class UsuarioService{
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+
+    public Usuario salvar(Usuario usuario){
+
+        return usuarioRepository.save(usuario);
+    }
+
+    public Usuario atualizar(Long id, Usuario usuario){
+    //TODO testar esse ID
+       Optional<Usuario> optionalUsuario = usuarioRepository.findById(usuario.getId());
+        if (optionalUsuario.isPresent()){
+            usuario.setId(id);
+            return usuarioRepository.save(usuario);
+        }
+        return null;
+    }
+
+    public Boolean remover(Long id){
+        if (usuarioRepository.existsById(id)){
+            usuarioRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Usuario> buscarTodos(){
+        return usuarioRepository.findAll();
+    }
 
     public Usuario buscarPeloId(Long id){
 
