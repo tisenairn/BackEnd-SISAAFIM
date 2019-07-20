166ab3a018dc6d476963cec62e506468becef06e
Lindson Naiam Lima de França <l1nds0n@hotmail.com>
2019-07-20 01:57:41 -0300

    Precisei comentar tudo, para reestruturar o banco de dados

--- src/main/java/br/gov/rn/saogoncalo/smtsis/services/LocalizacaoEnderecoService.java
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/services/LocalizacaoEnderecoService.java
@@ -13,53 +13,53 @@ import java.util.List;
 @Transactional
 public class LocalizacaoEnderecoService {
 
-    @Autowired
-    private LocalizacaoEnderecoRepository localizacaoEnderecoRepository;
-
-    @Autowired
-    private EnderecoService enderecoService;
-
-    public LocalizacaoEndereco salvar(LocalizacaoEndereco localizacaoEndereco, Long enderecoId) {
-        Endereco endereco = enderecoService.buscarPorId(enderecoId);
-        if (endereco != null ) {
-            if (buscarPorId(enderecoId) == null) {
-                localizacaoEndereco.setEndereco(endereco);
-                LocalizacaoEndereco localizacaoSalva = localizacaoEnderecoRepository.save(localizacaoEndereco);
-                return localizacaoSalva;
-            }
-        }
-        return null;
-    }
-
-    public List<LocalizacaoEndereco> buscarTodos() {
-        List<LocalizacaoEndereco> locaEndList = localizacaoEnderecoRepository.findAll();
-        return locaEndList.size() > 0 ? locaEndList : null;
-    }
-
-    public LocalizacaoEndereco buscarPorId(Long id) {
-        return locaEnderecoExiste(id) ? localizacaoEnderecoRepository.findById(id).get() : null;
-    }
-
-    private boolean locaEnderecoExiste(Long id){
-        return localizacaoEnderecoRepository.existsById(id);
-    }
-
-    public Boolean deletePorId(Long id) {
-        if (locaEnderecoExiste(id)){
-            localizacaoEnderecoRepository.deleteById(id);
-            return true;
-        }
-        return false;
-    }
-
-    public LocalizacaoEndereco atualizar(Long id, LocalizacaoEndereco localizacaoEndereco) {
-        if (buscarPorId(id) != null) {
-            LocalizacaoEndereco localizacaoEnderecoEncontrado = buscarPorId(id);
-            localizacaoEndereco.setId(localizacaoEnderecoEncontrado.getId());
-            localizacaoEndereco.setEndereco(localizacaoEnderecoEncontrado.getEndereco());
-            localizacaoEnderecoRepository.save(localizacaoEndereco);
-            return localizacaoEndereco;
-        }
-        return null;
-    }
+//    @Autowired
+//    private LocalizacaoEnderecoRepository localizacaoEnderecoRepository;
+//
+//    @Autowired
+//    private EnderecoService enderecoService;
+//
+//    public LocalizacaoEndereco salvar(LocalizacaoEndereco localizacaoEndereco, Long enderecoId) {
+//        Endereco endereco = enderecoService.buscarPorId(enderecoId);
+//        if (endereco != null ) {
+//            if (buscarPorId(enderecoId) == null) {
+//                localizacaoEndereco.setEndereco(endereco);
+//                LocalizacaoEndereco localizacaoSalva = localizacaoEnderecoRepository.save(localizacaoEndereco);
+//                return localizacaoSalva;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public List<LocalizacaoEndereco> buscarTodos() {
+//        List<LocalizacaoEndereco> locaEndList = localizacaoEnderecoRepository.findAll();
+//        return locaEndList.size() > 0 ? locaEndList : null;
+//    }
+//
+//    public LocalizacaoEndereco buscarPorId(Long id) {
+//        return locaEnderecoExiste(id) ? localizacaoEnderecoRepository.findById(id).get() : null;
+//    }
+//
+//    private boolean locaEnderecoExiste(Long id){
+//        return localizacaoEnderecoRepository.existsById(id);
+//    }
+//
+//    public Boolean deletePorId(Long id) {
+//        if (locaEnderecoExiste(id)){
+//            localizacaoEnderecoRepository.deleteById(id);
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//    public LocalizacaoEndereco atualizar(Long id, LocalizacaoEndereco localizacaoEndereco) {
+//        if (buscarPorId(id) != null) {
+//            LocalizacaoEndereco localizacaoEnderecoEncontrado = buscarPorId(id);
+//            localizacaoEndereco.setId(localizacaoEnderecoEncontrado.getId());
+//            localizacaoEndereco.setEndereco(localizacaoEnderecoEncontrado.getEndereco());
+//            localizacaoEnderecoRepository.save(localizacaoEndereco);
+//            return localizacaoEndereco;
+//        }
+//        return null;
+//    }
 }
