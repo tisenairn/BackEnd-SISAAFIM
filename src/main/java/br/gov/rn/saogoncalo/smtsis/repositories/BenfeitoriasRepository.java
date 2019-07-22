c315ad411425a9604d2fc198f32df6fde99c079f
Lindson França <l1nds0n@hotmail.com>
2019-07-22 13:45:33 -0300

    Refatorando Código

--- src/main/java/br/gov/rn/saogoncalo/smtsis/repositories/BenfeitoriasRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.rn.saogoncalo.smtsis.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.gov.rn.saogoncalo.smtsis.models.Benfeitorias;
-
-@Repository
-public interface BenfeitoriasRepository extends JpaRepository<Benfeitorias, Long> {}
