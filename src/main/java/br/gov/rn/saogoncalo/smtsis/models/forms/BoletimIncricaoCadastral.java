dc35304f3434afd2786cc05fd0d7b28cdf2fd070
Lindson França <l1nds0n@hotmail.com>
2019-07-23 15:39:19 -0300

    Instância de data no momento de salvar o bic

--- src/main/java/br/gov/rn/saogoncalo/smtsis/models/forms/BoletimIncricaoCadastral.java
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/models/forms/BoletimIncricaoCadastral.java
@@ -2,6 +2,8 @@ package br.gov.rn.saogoncalo.smtsis.models.forms;
 
 import br.gov.rn.saogoncalo.smtsis.models.contribuinte.Contribuinte;
 import br.gov.rn.saogoncalo.smtsis.models.administrative.Fiscal;
+import net.bytebuddy.implementation.bind.annotation.Default;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -16,10 +18,15 @@ public class BoletimIncricaoCadastral {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_BIC")
     private Long id;
+
+    @Value(value = "true")
     private boolean ativo;
 
-//    @Temporal(TemporalType.DATE)
-//    private Date data;
+    @Temporal(TemporalType.DATE)
+    private Date dataAbertura;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataFechadura;
 
 //  TODO Ao fim dos testes, as chaves estrangeiras do Boletim deverão ser do tipo Fetch Eager
 //    @OneToOne(fetch = FetchType.EAGER)
@@ -38,6 +45,22 @@ public class BoletimIncricaoCadastral {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Date getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(Date dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public Date getDataFechadura() {
+        return dataFechadura;
+    }
+
+    public void setDataFechadura(Date dataFechadura) {
+        this.dataFechadura = dataFechadura;
     }
 
     public boolean isAtivo() {
