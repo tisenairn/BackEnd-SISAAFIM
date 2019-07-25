79114d5d2bafd8fb640883034fb82fa112c19254
Lindson França <l1nds0n@hotmail.com>
2019-07-25 14:29:16 -0300

    Adicionei ao modelo de Endereço do contribuinte novas informações
    E algumas informações que não seriam utilizadas foram removidas

---
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/enums/UnidadeFederativa.java
@@ -0,0 +1,5 @@
+package br.gov.rn.saogoncalo.smtsis.enums;
+
+public enum UnidadeFederativa {
+    AC, AL, AM, AP, BA, CE, DF, ES, FN, GO, MA, MG, MS, MT, PA, PB, PE, PI, PR, RJ, RN, RO, RR, RS, SC, SE, SP, TO;
+}
--- src/main/java/br/gov/rn/saogoncalo/smtsis/models/contribuinte/ContribuinteEndereco.java
+++ src/main/java/br/gov/rn/saogoncalo/smtsis/models/contribuinte/ContribuinteEndereco.java
@@ -1,37 +1,39 @@
 package br.gov.rn.saogoncalo.smtsis.models.contribuinte;
 
+import br.gov.rn.saogoncalo.smtsis.enums.TipoLogradouro;
+import br.gov.rn.saogoncalo.smtsis.enums.TipoSubunidade;
+import br.gov.rn.saogoncalo.smtsis.enums.UnidadeFederativa;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "contribuintes_endereco")
 public class ContribuinteEndereco {
-
+// Entidade não obrigatória, ou seja, tudo nesta entidade pode ser nulo.
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_endereco")
     private Long id;
-    //@NotNull
-    //@NotEmpty
-    private String uf;
-    //@NotNull
-    //@NotEmpty
-    private String cidade;
-    //@NotNull
-    //@NotEmpty
-    private String bairro;
-    //@NotNull
-    //@NotEmpty
-    private String logradouro;
-    //@NotNull
-    //@NotEmpty
-    private String complemento;
-    //@NotNull
-    //@NotEmpty
+    private boolean possuiEnderecoCorrespondecia;
+
+    private String caixaPostal;
+    private String codLogradouro;
+    @Column(columnDefinition = "TINYINT(2) UNSIGNED")
+    private TipoLogradouro tipoLogradouro;
+    private String nomeLogradouro;
+    @Column(length = 5)
+    private int  numero;
+    @Column(length = 2)
+    private String bloco;
+    @Column(columnDefinition = "TINYINT(2) UNSIGNED")
+    private TipoSubunidade tipoSubunidade;
+    private int numeroSubunidade;
     private String cep;
-    //@NotNull
-    //@NotEmpty
-    private String numero;
+    private String bairro;
+    private String cidade;
+    @Column(columnDefinition = "TINYINT(2) UNSIGNED")
+    private UnidadeFederativa uf;
 
     public Long getId() {
         return id;
@@ -41,44 +43,76 @@ public class ContribuinteEndereco {
         this.id = id;
     }
 
-    public String getUf() {
-        return uf;
+    public boolean isPossuiEnderecoCorrespondecia() {
+        return possuiEnderecoCorrespondecia;
     }
 
-    public void setUf(String uf) {
-        this.uf = uf;
+    public void setPossuiEnderecoCorrespondecia(boolean possuiEnderecoCorrespondecia) {
+        this.possuiEnderecoCorrespondecia = possuiEnderecoCorrespondecia;
     }
 
-    public String getCidade() {
-        return cidade;
+    public String getCaixaPostal() {
+        return caixaPostal;
     }
 
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
+    public void setCaixaPostal(String caixaPostal) {
+        this.caixaPostal = caixaPostal;
     }
 
-    public String getBairro() {
-        return bairro;
+    public String getCodLogradouro() {
+        return codLogradouro;
     }
 
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
+    public void setCodLogradouro(String codLogradouro) {
+        this.codLogradouro = codLogradouro;
     }
 
-    public String getLogradouro() {
-        return logradouro;
+    public TipoLogradouro getTipoLogradouro() {
+        return tipoLogradouro;
     }
 
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
+    public void setTipoLogradouro(TipoLogradouro tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
     }
 
-    public String getComplemento() {
-        return complemento;
+    public String getNomeLogradouro() {
+        return nomeLogradouro;
     }
 
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
+    public void setNomeLogradouro(String nomeLogradouro) {
+        this.nomeLogradouro = nomeLogradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBloco() {
+        return bloco;
+    }
+
+    public void setBloco(String bloco) {
+        this.bloco = bloco;
+    }
+
+    public TipoSubunidade getTipoSubunidade() {
+        return tipoSubunidade;
+    }
+
+    public void setTipoSubunidade(TipoSubunidade tipoSubunidade) {
+        this.tipoSubunidade = tipoSubunidade;
+    }
+
+    public int getNumeroSubunidade() {
+        return numeroSubunidade;
+    }
+
+    public void setNumeroSubunidade(int numeroSubunidade) {
+        this.numeroSubunidade = numeroSubunidade;
     }
 
     public String getCep() {
@@ -89,39 +123,60 @@ public class ContribuinteEndereco {
         this.cep = cep;
     }
 
-    public String getNumero() {
-        return numero;
+    public String getBairro() {
+        return bairro;
     }
 
-    public void setNumero(String numero) {
-        this.numero = numero;
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
     }
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ContribuinteEndereco contribuinteEndereco = (ContribuinteEndereco) o;
-        return Objects.equals(id, contribuinteEndereco.id);
+    public String getCidade() {
+        return cidade;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public UnidadeFederativa getUf() {
+        return uf;
+    }
+
+    public void setUf(UnidadeFederativa uf) {
+        this.uf = uf;
     }
 
     @Override
     public String toString() {
         return "ContribuinteEndereco{" +
                 "id=" + id +
-                ", uf='" + uf + '\'' +
-                ", cidade='" + cidade + '\'' +
-                ", bairro='" + bairro + '\'' +
-                ", logradouro='" + logradouro + '\'' +
-                ", complemento='" + complemento + '\'' +
+                ", possuiEnderecoCorrespondecia=" + possuiEnderecoCorrespondecia +
+                ", caixaPostal='" + caixaPostal + '\'' +
+                ", codLogradouro='" + codLogradouro + '\'' +
+                ", tipoLogradouro=" + tipoLogradouro +
+                ", nomeLogradouro='" + nomeLogradouro + '\'' +
+                ", numero=" + numero +
+                ", bloco='" + bloco + '\'' +
+                ", tipoSubunidade=" + tipoSubunidade +
+                ", numeroSubunidade=" + numeroSubunidade +
                 ", cep='" + cep + '\'' +
-                ", numero='" + numero + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", cidade='" + cidade + '\'' +
+                ", uf=" + uf +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContribuinteEndereco that = (ContribuinteEndereco) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
