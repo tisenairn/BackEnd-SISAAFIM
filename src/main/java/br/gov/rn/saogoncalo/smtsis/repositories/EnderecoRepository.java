7ce268754f2732fc2c25d31f4a9a7534e980a32a
Lindson França <l1nds0n@hotmail.com>
2019-08-05 11:14:48 -0300

    Revisão de itens desnecessários no REST

--- src/main/java/br/gov/rn/saogoncalo/smtsis/repositories/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.gov.rn.saogoncalo.smtsis.repositories;
-
-import br.gov.rn.saogoncalo.smtsis.models.imovel.InfoGerais.ImovelEndereco;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<ImovelEndereco, Long> {}
